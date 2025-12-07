06489349eb869d322b3ad3b0ef6edbfe41eae36e
S M Hossain Mazba Alvi <85153673+Alvi100@users.noreply.github.com>
2025-12-07 11:51:17 +0600

    Create input.js

---
+++ input.js
@@ -0,0 +1,25 @@
+function myFunction(){
+    document.getElementById("name").innerHTML= prompt("Enter Your Name: [ max-22 Character ] ");
+    document.getElementById("roll").innerHTML= prompt("Enter Your Roll:");
+    document.getElementById("reg").innerHTML= prompt("Enter DU Reg Number:");
+    document.getElementById("sname").style.display="none";
+    
+ }
+
+ function myCorse(){
+    document.getElementById("course").style.appearance="none";
+ }
+
+ function myTeacher(){
+    document.getElementById("teacher").style.appearance="none";
+ }
+
+ function mySession(){
+    document.getElementById("session").style.appearance="none";
+ }
+
+//  function myPrint(){
+//     window.print();
+//     document.getElementById("print").style.display="none";
+  
+//   }
